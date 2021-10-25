6f6b88b6aaf0cfdb5cda58122b943524e3bf2015
Cesar Alcancio <cesar.alcancio@gmail.com>
2021-10-25 10:43:58 -0300

    Added tests

--- src/service_clojure_offers/common/utils.clj
+++ src/service_clojure_offers/common/utils.clj
@@ -6,3 +6,17 @@
 
 (defn uuid
   [] (UUID/randomUUID))
+
+(defmacro run-threads
+  [t f]
+  `(dotimes [i# ~t]
+     (.start
+       (Thread. (fn [] ~f)))))
+
+(defn dummy-offer
+  [customer-id]
+  {:id          (uuid)
+   :customer-id customer-id
+   :status      :available
+   :amount      10M
+   :created-at  (now)})
---
+++ test/service_clojure_offers/datomic/solution_test.clj
@@ -0,0 +1,63 @@
+(ns service-clojure-offers.datomic.solution-test
+  (:require [clojure.test :refer :all]
+            [service-clojure-offers.datomic.config :as config]
+            [service-clojure-offers.datomic.offers :as d.offers]
+            [service-clojure-offers.controllers.offers :as c.offers]
+            [service-clojure-offers.common.utils :as utils]
+            [clojure.pprint :as pp]))
+
+(defn run-before-test [f]
+  ; prepare database
+  (config/delete-database! config/base-uri config/db-name-offers)
+  (config/create-database! config/base-uri config/db-name-offers)
+  (let [conn (config/connect! config/base-uri config/db-name-offers)]
+    (config/list-databases! config/base-uri)
+    ; process schema
+    (config/create-schema! conn d.offers/offer-schema)
+    ; delete all data
+    (doseq [offer (d.offers/find-all! (config/db! conn))]
+      (d.offers/delete-by-id! conn (:id offer))))
+  ; run tests
+  (f))
+
+(use-fixtures :once run-before-test)
+
+(deftest create-offer-v2-test
+  (let [customer-id (utils/uuid)
+        first-offer (utils/dummy-offer customer-id)
+        conn (config/connect! config/base-uri config/db-name-offers)
+        datomic {:conn conn :db (config/db! conn)}
+        _ (c.offers/create-offer-v2! datomic first-offer)
+        offers (d.offers/find-by-customer-id! (config/db! conn) customer-id)]
+    (is (= (count offers) 1))))
+
+(deftest validate-first-solution-test
+  (let [customer-id (utils/uuid)
+        conn (config/connect! config/base-uri config/db-name-offers)]
+    (utils/run-threads
+      100
+      (let [offer (utils/dummy-offer customer-id)
+            datomic {:conn conn :db (config/db! conn)}]
+        (c.offers/create-offer-v2! datomic offer)))
+    (Thread/sleep 1000)
+    (is (= (count (d.offers/find-by-customer-id! (config/db! conn) customer-id)) 3))))
+
+(deftest create-offer-v3-test
+  (let [customer-id (utils/uuid)
+        first-offer (utils/dummy-offer customer-id)
+        conn (config/connect! config/base-uri config/db-name-offers)
+        datomic {:conn conn :db (config/db! conn)}
+        _ (c.offers/create-offer-v3! datomic first-offer)
+        offers (d.offers/find-by-customer-id! (config/db! conn) customer-id)]
+    (is (= (count offers) 1))))
+
+(deftest validate-second-solution-test
+  (let [customer-id (utils/uuid)
+        conn (config/connect! config/base-uri config/db-name-offers)]
+    (utils/run-threads
+      100
+      (let [offer (utils/dummy-offer customer-id)
+            datomic {:conn conn :db (config/db! conn)}]
+        (c.offers/create-offer-v3! datomic offer)))
+    (Thread/sleep 1000)
+    (is (= (count (d.offers/find-by-customer-id! (config/db! conn) customer-id)) 3))))
